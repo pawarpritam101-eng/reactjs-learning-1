e09ca3c1baf0fd9bc4fd578641d4f490dc99f900
pawarpritam101-eng <pawarpritam101@gmail.com>
2026-01-13 11:25:29 +0000

    rounter learning

--- routerweb/src/components/Header.jsx
+++ routerweb/src/components/Header.jsx
@@ -15,11 +15,11 @@ function Header() {
     font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5
     mr-2 focus:outline-none'>Log in</Link>
     <Link to="#" className='text-white bg-orange-700 hover:bg-orange-700 focus:ring-4 focus:ring-gray-300 
-    font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:px-2.5 mr-2 focus:outline-none'>Get Started</Link>
+    font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none'>Get Started</Link>
    </div>
   
-  <div className='hidden justify-between itens-center w-full lg:flex lg:w-auto lg:order-1' id='mobile-menu-2'>
-     <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0'>
+  <div className='hidden justify-between itens-center w-full lg:flex lg:w-auto lg:order-1' id='mobile-menu-4'>
+     <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-2'>
       <li>
         <NavLink to="/" className={({isActive}) =>`block py-2 pr-4 pl-3 duration-200 ${isActive?"text-orange-700":"text-gray-700"} 
         border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}>
--- routerweb/src/main.jsx
+++ routerweb/src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import {RouterProvider,Route, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/Header.jsx'
+import Home from './components/Home.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}></Route>
+        <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+    </Route>
+    
   )
 )
 
