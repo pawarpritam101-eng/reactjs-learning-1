c21c11181b74c964717823eb544c5f370c082a3b
Pritam Pawar <pawarpritam101@gmail.com>
2026-02-03 11:39:53 +0000

    header

--- sharemarket/src/Homepage.jsx
+++ sharemarket/src/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 
+
 function Homepage() {
   return (
   <>
--- sharemarket/src/components/Header.jsx
+++ sharemarket/src/components/Header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
   {/* <div className="w-70 h-125 border-2 border-blue-500 p-4"> */}
   
   <div className="flex flex-col gap-7 py-16">
-    <div className="border-2 rounded-sm border-gray-200 p-2 text-center  text-white">
+    <div className="border-2 rounded-2xl border-gray-200 p-2 text-center  text-white w-72.5 h-10.5  px-3.5 py-2.2">
+       <span className='border rounded-sm w-8.5 h-4.5 p-2.5 '>New </span>
       Latest Integration Just Allowed
     </div>
 
